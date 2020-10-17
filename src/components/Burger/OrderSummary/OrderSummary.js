9d28996c96b6ce58f6793ff3caf7e801c4186357
Hassan <hassan.abbascs@gmail.com>
2020-10-17 13:43:21 +0300

    feat: add modal & backdrop components

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+// import Button from '../../../components/UI/Button/Button';
+
+class OrderSummary extends Component {
+	render() {
+		const ingredients = this.props.ingredients;
+
+		const ingredientSummary = Object.keys(ingredients).map((igKey) => {
+			return (
+				<li key={igKey}>
+					<span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+					{ingredients[igKey]}
+				</li>
+			);
+		});
+		return (
+			<React.Fragment>
+				<h3>Your order</h3>
+				<p>A delicious burger with the following ingredients: </p>
+				<ul>{ingredientSummary}</ul>
+				<p>
+					{/* <strong>Total price: ${this.props.price.toFixed(2)}</strong> */}
+				</p>
+				<p>Continue to checkout?</p>
+				{/* <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+					CANCEL
+				</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>
+					CONTINUE
+				</Button> */}
+			</React.Fragment>
+		);
+	}
+}
+
+export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -20,6 +22,7 @@ class BurgerBuilder extends Component {
 		},
 		totalPrice: 4,
 		purchasable: false,
+		purchasing: false,
 	};
 
 	updatePurchaseState(ingredients) {
@@ -70,6 +73,33 @@ class BurgerBuilder extends Component {
 		this.updatePurchaseState(updatedIngredients);
 	};
 
+	purchaseHandler = () => {
+		this.setState({ purchasing: true });
+	};
+
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	};
+
+	purchaseContinueHandler = async () => {
+		// passing the ingredients as query params
+		const queryParams = [];
+		for (let i in this.state.ingredients) {
+			queryParams.push(
+				`${encodeURIComponent(i)}=${encodeURIComponent(
+					this.state.ingredients[i]
+				)}`
+			);
+		}
+		queryParams.push(`price=${this.state.totalPrice}`);
+		const queryString = queryParams.join('&');
+
+		this.props.history.push({
+			pathname: 'checkout',
+			search: '?' + queryString,
+		});
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients,
@@ -81,6 +111,12 @@ class BurgerBuilder extends Component {
 
 		return (
 			<React.Fragment>
+				<Modal
+					show={this.state.purchasing}
+					modalClosed={this.purchaseCancelHandler}
+				>
+					<OrderSummary ingredients={this.state.ingredients} />
+				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
