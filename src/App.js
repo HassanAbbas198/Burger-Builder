47aa01b1776ab5fa9e92d7adbebf0d0e7ac16a83
Hassan <hassan.abbascs@gmail.com>
2020-10-19 18:43:30 +0300

    feat: add Checkout component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+// import Checkout from './containers/Checkout/Checkout';
 
 function App() {
 	return (
