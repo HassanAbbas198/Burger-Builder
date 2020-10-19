9e0753839cd9a5d941c1583e5cbadd4e2d9343c7
Hassan <hassan.abbascs@gmail.com>
2020-10-19 18:51:22 +0300

    feat: add routing & routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-// import Checkout from './containers/Checkout/Checkout';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
 	return (
-		<div>
-			<Layout>
-				<BurgerBuilder />
-			</Layout>
-		</div>
+		<Router>
+			<div>
+				<Layout>
+					<Switch>
+						<Route path="/checkout" component={Checkout} />
+						<Route path="/" exact component={BurgerBuilder} />
+					</Switch>
+				</Layout>
+			</div>
+		</Router>
 	);
 }
 
