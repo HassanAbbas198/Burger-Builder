d1235a8ef557d01610cf52e15499ac06407446da
Hassan <hassan.abbascs@gmail.com>
2020-10-19 22:58:51 +0300

    feat: pass data via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,28 +94,45 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = async () => {
-		this.setState({ loading: true });
+		const queryParams = [];
 
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'hassan abbas',
-				address: {
-					street: 'habla',
-					country: 'Lebanon',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		};
-
-		try {
-			await axios.post('/orders.json', order);
-			this.setState({ loading: false, purchasing: false });
-		} catch (error) {
-			this.setState({ loading: false, purchasing: false });
+		for (let i in this.state.ingredients) {
+			queryParams.push(
+				`${encodeURIComponent(i)}=${encodeURIComponent(
+					this.state.ingredients[i]
+				)}`
+			);
 		}
+
+		const queryString = queryParams.join('&');
+
+		this.props.push({
+			pathname: '/checkout',
+			search: `?${queryString}`,
+		});
+
+		// this.setState({ loading: true });
+
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'hassan abbas',
+		// 		address: {
+		// 			street: 'habla',
+		// 			country: 'Lebanon',
+		// 		},
+		// 		email: 'test@test.com',
+		// 	},
+		// 	deliveryMethod: 'fastest',
+		// };
+
+		// try {
+		// 	await axios.post('/orders.json', order);
+		// 	this.setState({ loading: false, purchasing: false });
+		// } catch (error) {
+		// 	this.setState({ loading: false, purchasing: false });
+		// }
 	};
 
 	render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,17 +3,38 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
 	state = {
-		ingredients: {
-			salad: 1,
-			meat: 1,
-			cheese: 1,
-			bacon: 1,
-		},
+		ingredients: null,
 	};
+
+	componentDidMount() {
+		const query = new URLSearchParams(this.props.location.search);
+		const ingredients = {};
+
+		for (let param of query.entries()) {
+			ingredients[param[0]] = +param[1];
+		}
+
+		this.setState({
+			ingredients: ingredients,
+		});
+	}
+
+	checkoutCanceledHandler = () => {
+		this.props.history.goBack();
+	};
+
+	checkoutContinuedHandler = () => {
+		this.props.history.replace('/checkout/contact-data');
+	};
+
 	render() {
 		return (
 			<div>
-				<CheckoutSummary ingredients={this.state.ingredients} />
+				<CheckoutSummary
+					ingredients={this.state.ingredients}
+					checkoutCanceled={this.checkoutCanceledHandler}
+					checkoutContinued={this.checkoutContinuedHandler}
+				/>
 			</div>
 		);
 	}
