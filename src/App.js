aaa918d0e35694432005845b81dc6426d774fd04
Hassan <hassan.abbascs@gmail.com>
2020-10-23 20:16:54 +0300

    feat: guard routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as actions from './store/actions/index';
@@ -16,18 +21,30 @@ class App extends Component {
 		this.props.onTryAutoSignup();
 	}
 	render() {
+		let routes = (
+			<Switch>
+				<Route path="/auth" component={Auth} />
+				<Route path="/" exact component={BurgerBuilder} />
+				<Redirect to="/" />
+			</Switch>
+		);
+
+		if (this.props.isAuth) {
+			routes = (
+				<Switch>
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/orders" component={Orders} />
+					<Route path="/auth" component={Auth} />
+					<Route path="/logout" component={Logout} />
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</Switch>
+			);
+		}
 		return (
 			<Router>
 				<div>
-					<Layout>
-						<Switch>
-							<Route path="/checkout" component={Checkout} />
-							<Route path="/orders" component={Orders} />
-							<Route path="/auth" component={Auth} />
-							<Route path="/logout" component={Logout} />
-							<Route path="/" exact component={BurgerBuilder} />
-						</Switch>
-					</Layout>
+					<Layout>{routes}</Layout>
 				</div>
 			</Router>
 		);
